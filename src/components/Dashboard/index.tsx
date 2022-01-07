b02558f1396e436a5474131a4a09360429b57cb5
Rafael Reis <rafa.araujoreis@gmail.com>
2022-01-07 08:59:48 -0300

    Selecionar temperatura

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,18 +1,45 @@
+import { FormEvent, useState } from "react";
 import { Container, RadioBox } from "./styles";
 
+
+
+
 export function Dashboard() {
+    const [temperature, setTemperature] = useState(0);
+    const [typeTemperature, setTypeTemperature] = useState('Celsius')
+
+    function handleSelectedTemperature(typeTemperature: string) {
+        setTypeTemperature(typeTemperature);
+        onConvertTemperature();
+    }
+
+    function onSetTemperature(temperature: number) {
+        setTemperature(temperature);
+        onConvertTemperature();
+    }
+
+    function onConvertTemperature() {
+        console.log('temperature: ', temperature, 'type: ', typeTemperature)
+    }
+
     return (
         <Container>
             <p>Qual a temperatura deseja converter?</p>
             <RadioBox>
-                <button type="button">Fahrenheit</button>
-                <button type="button">Celsius</button>
-                <button type="button">Kelvin</button>
+                <button type="button" onClick={
+                    () => handleSelectedTemperature('Celsius')
+                }>Celsius</button>
+                <button type="button" onClick={
+                    () => handleSelectedTemperature('Fahrenheit')
+                }>Fahrenheit</button>
+                <button type="button" onClick={
+                    () => handleSelectedTemperature('Kelvin')
+                }>Kelvin</button>
             </RadioBox>
             <input
                 type="number"
-                placeholder="Quantos Celsius deseja converter?"
-            // onChange={}
+                placeholder={`Quantos ${typeTemperature} deseja converter?`}
+                onChange={(event) => onSetTemperature(Number(event.target.value))}
             />
         </Container>
     );
