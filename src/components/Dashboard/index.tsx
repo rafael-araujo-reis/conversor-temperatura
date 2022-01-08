cb3266299e3fdd62c2beb94a9b1c487753915dde
Rafael Reis <rafa.araujoreis@gmail.com>
2022-01-07 22:23:37 -0300

    Conversor

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,8 +1,5 @@
-import { FormEvent, useState } from "react";
+import { useState } from "react";
 import { Container, RadioBox } from "./styles";
-
-
-
 
 export function Dashboard() {
     const [temperature, setTemperature] = useState(0);
@@ -10,16 +7,40 @@ export function Dashboard() {
 
     function handleSelectedTemperature(typeTemperature: string) {
         setTypeTemperature(typeTemperature);
-        onConvertTemperature();
+        onConvertTemperature(typeTemperature);
+        console.log(`type: ${typeTemperature} temperature: ${temperature}`)
     }
 
     function onSetTemperature(temperature: number) {
         setTemperature(temperature);
-        onConvertTemperature();
+        onConvertTemperature(typeTemperature);
     }
 
-    function onConvertTemperature() {
-        console.log('temperature: ', temperature, 'type: ', typeTemperature)
+    function onConvertTemperature(type: string) {
+        let celsius, fahrenheit, kelvin = 0;
+
+        if (type === 'Celsius') {
+            celsius = temperature;
+
+            fahrenheit = celsius * 9 / 5 + 32;
+            kelvin = celsius + 273.15;
+
+            console.log(`celsius ${celsius} -> fahrenheit ${fahrenheit} kelvin ${kelvin}`)
+        } else if (type === 'Fahrenheit') {
+            fahrenheit = temperature;
+
+            celsius = (fahrenheit - 32) * 5 / 9;
+            kelvin = (fahrenheit - 32) * 5 / 9 + 273.15;
+
+            console.log(`fahrenheit ${fahrenheit} -> celsius ${celsius} kelvin ${kelvin}`)
+        } else {
+            kelvin = temperature;
+
+            celsius = kelvin - 273.15;
+            fahrenheit = (kelvin - 273.15) * 9 / 5 + 32;
+
+            console.log(`kelvin ${kelvin} -> celsius ${celsius} fahrenheit ${fahrenheit}`)
+        }
     }
 
     return (
@@ -38,6 +59,7 @@ export function Dashboard() {
             </RadioBox>
             <input
                 type="number"
+
                 placeholder={`Quantos ${typeTemperature} deseja converter?`}
                 onChange={(event) => onSetTemperature(Number(event.target.value))}
             />
